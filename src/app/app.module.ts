1b59251654dbc2f4111ea6ca575beaa6393a7d60
Anderson Menezes <anderson.stifip@gmail.com>
2022-11-23 01:31:44 -0300

    adjustment import app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponentsModule } from './components/product-components.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ProductComponentsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
