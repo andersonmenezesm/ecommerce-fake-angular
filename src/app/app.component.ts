274c4b9a58eb14344bbeb717e5b435dd81b4dcc9
Anderson Menezes <anderson.stifip@gmail.com>
2022-11-23 01:32:04 -0300

    adjustment app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { Product } from './models/product.model';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit, OnDestroy {
+  products$: Observable<Product[]> | undefined
+  subscribe: Subscription | undefined
+  productsList: Product[] | undefined
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.products$ = this.productService.getListAllProducts()
+    this.getAllProducts()
+  }
+
+  getAllProducts() {
+    // this.subscribe = this.products$?.subscribe()
+  }
+
+  ngOnDestroy(): void {
+    // this.subscribe?.unsubscribe
+  }
 }
